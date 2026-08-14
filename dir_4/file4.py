ff5f34e065fba2eaffcffd4b3d5b734c6568d727
Iryna <shevchenko.irina2018@gmail.com>
2026-08-14 17:38:11 +0300

    Change info in files in folder 'dir_4' in branch 'newbranch'

--- dir_4/file4.py
+++ dir_4/file4.py
@@ -1,1 +1,8 @@
-print("Hello 2!")
+def search_student(user_search_name):
+    for student, info in students_list.items():
+        if user_search_name == student:
+            for key, value in info.items():
+                print(f"{key}: {value}")
+        else:
+            print(f"{student} is not in the list")
+user_search_name = input("Enter student name: ")
